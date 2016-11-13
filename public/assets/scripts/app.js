81a08bb417620a65b199c222909c96f75d7590b0
sfr <sfr@sfr.ninja>
2016-11-13 14:49:46 +0100

    Actually list and page between requests in frontend

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,14 +1,39 @@
-angular.module("app", [])
-
+angular
+    .module("app", [])
+    .config(function($logProvider) {
+        $logProvider.debugEnabled(true);
+    })
     .controller("AppController", function ($scope, $http) {
         $scope.token = {};
+        $scope.requests = [];
+        $scope.currentRequestIndex = 0;
+        $scope.currentRequest = {};
+        $scope.hasRequests = false;
+        $scope.domain = window.location.href;
 
+        $scope.setCurrentRequest = (function(value) {
+            $scope.currentRequestIndex = value;
+            $scope.currentRequest = $scope.requests.data[value];
+            console.log('Current:', value);
+            console.log($scope.currentRequest);
+        });
+
+        // Initialize Clipboard copy button
+        new Clipboard('#copyTokenUrl');
+
+        // Let's start by grabbing a token
         $http.post('token')
             .then(function(response, status) {
                 $scope.token = response.data;
                 console.log($scope.token.uuid);
-                console.log(status);
             }, function(response) {
-                console.log('Error: ' + response.data);
+                console.log('Could not get token: ' + response.data);
+            });
+
+        $http.get('/token/b874bafd-67e4-408c-a7b5-bb2c21ad91ba/requests')
+            .then(function (response, status) {
+                $scope.requests = response.data;
+                $scope.setCurrentRequest($scope.currentRequestIndex);
+                $scope.hasRequests = true;
             });
     });
--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -2,24 +2,18 @@
 <html>
 <head>
     <title>Webhook Tester</title>
+    <!-- Libraries -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-2.2.2.min.js" async></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
-            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-            crossorigin="anonymous" async></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js"></script>
+
+
+    <!-- App -->
     <script src="assets/scripts/app.js"></script>
-
-
     <link href="assets/css/main.css" rel="stylesheet">
 </head>
 <body ng-app="app" ng-controller="AppController">
-
-<script type="application/javascript">
-
-
-</script>
 
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
@@ -34,10 +28,16 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <div class="nav navbar-left navbar-form">
-                <input type="text" class="form-control click-select"
-                       style="width: auto;"
-                       value="http://localhost/{{ token.uuid }}">
+                <div class="form-group">
+                    <label for="tokenUrl" style="color: #999">
+                        Send webhooks to: &nbsp;
+                    </label>
 
+                    <input id="tokenUrl" type="text" class="form-control click-select"
+                           style="width: 400px;"
+                           value="{{ domain }}{{ token.uuid }}">
+                </div>
+                <button class="btn btn-default" id="copyTokenUrl" data-clipboard-target="#tokenUrl">Copy</button>
             </div>
         </div>
     </div>
@@ -46,87 +46,55 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
-            <p class="sidebar-header">Requests {{angulartest}}</p>
+            <p class="sidebar-header">Requests</p>
             <ul class="nav nav-sidebar">
-                <li class="active">
+                <li ng-repeat="(key, value) in requests.data"
+                    ng-class="currentRequestIndex === key ? 'active' : ''"
+                    ng-click="setCurrentRequest(key)">
                     <a href="#">
-                       #3 POST 19:46:01 2.9kb
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                       #2 POST 19:45:40 0.9kb
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                       #1 POST 19:40:24 0.9kb
+                       #{{ key }} {{ value.method }} {{ value.created_at }} {{ value.ip }}
                     </a>
                 </li>
             </ul>
         </div>
         <div id="request" class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-            <div class="table-responsive">
+            <div class="table-responsive" ng-show="hasRequests">
                 <table class="table">
                     <tbody>
                     <tr>
                         <td><b>Date</b></td>
-                        <td id="req-date">20 March 2016, 19:36:00</td>
+                        <td id="req-date">{{ currentRequest.created_at }}</td>
                         <td><b>URL</b></td>
-                        <td id="req-url">http://localhost:8000/9014f885-6ad5-4734-a1e7-ac9a164cff72</td>
+                        <td id="req-url">{{ currentRequest.url }}</td>
                         <td><b>Method</b></td>
-                        <td id="req-method">POST</td>
+                        <td id="req-method">{{ currentRequest.method }}</td>
                         <td><b>IP/Host</b></td>
-                        <td id="req-ip">127.0.0.1</td>
+                        <td id="req-ip">{{ currentRequest.hostname }} ({{ currentRequest.ip }})</td>
                     </tr>
                     <tr>
                         <td><b>Headers</b></td>
                         <td colspan="7" id="req-headers">
-                            <b>Host</b>: localhost<br>
-                            <b>Connection</b>: close<br>
-                            <b>User-Agent</b>: Paw/2.3.2 (Macintosh; OS X/10.11.3) GCDHTTPRequest
+                            <p ng-repeat="(headerName, values) in currentRequest.headers">
+                                <strong>{{ headerName }}:</strong>
+                                <span ng-repeat="value in values">{{value}}{{$last ? '' : ', '}}</span>
+                            </p>
 
                         </td>
                     </tr>
                     </tbody>
                 </table>
 <pre id="req-content">
-{
-  "lead_type_id": "2acb4738-dcd6-4e6e-aeb6-ed45ddeb9ab6",
-  "team_id": 1,
-  "lead_source": "Website",
-  "b2c": true,
-  "account": {
-    "fixed_fields": [
-      {
-        "name": "name",
-        "value": "Microsoft"
-      }
-    ]
-  },
-  "contact": {
-    "fixed_fields": [
-      {
-        "name": "first_name",
-        "value": "Bill"
-      },
-      {
-        "name": "last_name",
-        "value": "Gates"
-      }
-    ]
-  }
-}
+{{ (currentRequest.content == '' ? '(no content)' : currentRequest.content) }}
 </pre>
 
             </div>
         </div>
     </div>
 </div>
-
-<script src="assets/scripts/main.js"></script>
-
-
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+        crossorigin="anonymous" async defer></script>
+<script src="https://cdn.jsdelivr.net/clipboard.js/1.5.13/clipboard.min.js"></script>
 
 </body>
 </html>
