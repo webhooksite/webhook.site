d8de2d0647f0e6e41c0c5a96dfa440d5f383b2d8
sfr <sfr@sfr.ninja>
2016-11-15 18:55:20 +0100

    Add GA

--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -130,6 +130,14 @@
         integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
         crossorigin="anonymous" async defer></script>
 <script src="assets/scripts/libs/clipboard.min.js"></script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+    ga('create', 'UA-5230636-9', 'auto');
+    ga('send', 'pageview');
+</script>
 </body>
 </html>
