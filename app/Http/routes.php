24bc12a6ddd8f04162fb821a4a276de460f2df2d
sfr <sfr@sfr.ninja>
2016-11-15 20:25:25 +0100

    #2 Allow extra path

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,11 +4,14 @@
 
 Route::pattern('uuid', '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}');
 Route::pattern('statusCode', '[1-5][0-9][0-9]');
+Route::pattern('any', '.*');
+
 
 Route::get('/', 'HomeController@index');
 
 Route::group(['middleware' => ['api']], function () {
     Route::any('{uuid}/{statusCode?}', 'RequestController@create');
+    Route::any('{uuid}/{any}', 'RequestController@create');
     Route::get('token/{uuid}/requests', 'RequestController@all');
 
     Route::post('token', 'TokenController@create');
